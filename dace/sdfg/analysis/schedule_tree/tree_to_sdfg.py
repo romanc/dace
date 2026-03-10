c08a91cd1f3195588dcc9bb908fdb5d39834ffcb
Roman Cattaneo <romanc@users.noreply.github.com>
2026-03-10 09:28:31 +0100

    pref: avoid calls to getdebuginfo() in stree -> sdfg

--- dace/sdfg/analysis/schedule_tree/tree_to_sdfg.py
+++ dace/sdfg/analysis/schedule_tree/tree_to_sdfg.py
@@ -2,6 +2,7 @@
 import copy
 from collections import defaultdict
 from dace import symbolic
+from dace.dtypes import DebugInfo
 from dace.memlet import Memlet
 from dace.sdfg import nodes, memlet_utils as mmu
 from dace.sdfg.sdfg import SDFG, ControlFlowRegion, InterstateEdge
@@ -312,9 +313,12 @@ class StreeToSDFG(tn.ScheduleNodeVisitor):
         assert len(self._dataflow_stack) == dataflow_stack_size
 
         # insert nested SDFG
-        nsdfg = self._current_state.add_nested_sdfg(sdfg=inner_sdfg,
-                                                    inputs=connectors["inputs"],
-                                                    outputs=connectors["outputs"])
+        nsdfg = self._current_state.add_nested_sdfg(
+            sdfg=inner_sdfg,
+            inputs=connectors["inputs"],
+            outputs=connectors["outputs"],
+            debuginfo=DebugInfo(4242),
+        )
         # connect nested SDFG to surrounding map scope
         assert self._dataflow_stack
         map_entry, to_connect = self._dataflow_stack[-1]
@@ -460,7 +464,7 @@ class StreeToSDFG(tn.ScheduleNodeVisitor):
 
                 # cache local read access
                 assert memlet_data not in access_cache
-                access_cache[memlet_data] = self._current_state.add_read(memlet_data)
+                access_cache[memlet_data] = self._current_state.add_read(memlet_data, DebugInfo(4242))
                 cached_access = access_cache[memlet_data]
                 self._current_state.add_memlet_path(cached_access,
                                                     map_entry,
@@ -529,7 +533,7 @@ class StreeToSDFG(tn.ScheduleNodeVisitor):
             #  A[i] = tasklet(A[i])
             if name not in access_cache or self._current_state.out_degree(access_cache[name]) > 0:
                 # cache write access into access_cache
-                write_access_node = self._current_state.add_write(name)
+                write_access_node = self._current_state.add_write(name, DebugInfo(4242))
                 access_cache[name] = write_access_node
 
             access_node = access_cache[name]
@@ -609,7 +613,7 @@ class StreeToSDFG(tn.ScheduleNodeVisitor):
 
             # cache local read access
             assert memlet.data not in cache
-            cache[memlet.data] = self._current_state.add_read(memlet.data)
+            cache[memlet.data] = self._current_state.add_read(memlet.data, DebugInfo(4242))
             cached_access = cache[memlet.data]
             self._current_state.add_memlet_path(cached_access, tasklet, dst_conn=name, memlet=memlet)
 
@@ -623,7 +627,7 @@ class StreeToSDFG(tn.ScheduleNodeVisitor):
             # A[1] = tasklet(A[1])
             if memlet.data not in cache or self._current_state.out_degree(cache[memlet.data]) > 0:
                 # cache write access node
-                write_access_node = self._current_state.add_write(memlet.data)
+                write_access_node = self._current_state.add_write(memlet.data, DebugInfo(4242))
                 cache[memlet.data] = write_access_node
 
             access_node = cache[memlet.data]
@@ -666,11 +670,14 @@ class StreeToSDFG(tn.ScheduleNodeVisitor):
 
         # assumption source access may or may not yet exist (in this state)
         src_name = node.memlet.data
-        source = access_cache[src_name] if src_name in access_cache else self._current_state.add_read(src_name)
+        source = access_cache[src_name] if src_name in access_cache else self._current_state.add_read(
+            src_name,
+            DebugInfo(4242),
+        )
 
         # assumption: target access node doesn't exist yet
         assert node.target not in access_cache
-        target = self._current_state.add_write(node.target)
+        target = self._current_state.add_write(node.target, DebugInfo(4242))
 
         self._current_state.add_memlet_path(source, target, memlet=node.memlet)
 
